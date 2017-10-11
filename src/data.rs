f7be47c0633da4093d09b848ff994f11644f2c7c
Rick (slikrick) <rickyeckberg@gmail.com>
2017-10-11 11:19:43 -0400

    Fixed offset multiplication (or lack thereof)
    Added cutoff, needs some fixing up
    Added some TODOs
    Added struct impls

--- src/data.rs
+++ src/data.rs
@@ -5,47 +5,56 @@ use csv;
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Alltime {
     pub _total: u64,
-    users: Vec<User>
+    users: Vec<User>,
 }
 
 impl Alltime {
-	pub fn users(&self) -> &Vec<User> {
-		&self.users
-	}
+    pub fn users(&self) -> &Vec<User> {
+        &self.users
+    }
+
+    pub fn into_users(self) -> Vec<User> {
+        self.users
+    }
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct User {
     username: String,
-    points: u64
+    pub points: u64,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
-	channel: String
+    channel: String,
+    cutoff: Option<u64>,
 }
 
-impl ActualConfig {
-	pub fn into_channel(self) -> String {
-		self.info.channel
-	}
-}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ActualConfig {
-	info: Config,
+    info: Config,
 }
 
+impl ActualConfig {
+    pub fn channel(&self) -> &str {
+        &self.info.channel
+    }
+
+    pub fn cutoff(&self) -> Option<u64> {
+        self.info.cutoff
+    }
+}
 
 #[derive(Debug, Error)]
 pub enum Error {
-	/// There was an error sending a request to the site, 
-	/// possibly check your internet connection
-	Reqwest(reqwest::Error),
-	/// There was an error with the I/O of your system
-	Io(::std::io::Error),
-	/// Unable to read config
-	TomlDeserialized(toml::de::Error),
-	/// Error with CSV info
-	Csv(csv::Error),
+    /// There was an error sending a request to the site,
+    /// possibly check your internet connection
+    Reqwest(reqwest::Error),
+    /// There was an error with the I/O of your system
+    Io(::std::io::Error),
+    /// Unable to read config
+    TomlDeserialized(toml::de::Error),
+    /// Error with CSV info
+    Csv(csv::Error),
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
 #![allow(unused_variables)]
-#[macro_use]
-extern crate serde_derive;
+extern crate chrono;
+extern crate csv;
 #[macro_use]
 extern crate derive_error;
 #[macro_use]
 extern crate log;
+extern crate reqwest;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
 extern crate simplelog;
 extern crate toml;
-extern crate reqwest;
-extern crate serde_json;
-extern crate csv;
-extern crate chrono;
 
 use chrono::offset::Utc;
 use csv::WriterBuilder;
 use std::fs::File;
 
 mod data;
-use data::{Alltime, ActualConfig, Error, User};
+use data::{ActualConfig, Alltime, Error, User};
 
 const BASE_URL: &str = "https://api.streamelements.com/kappa/v1";
 
@@ -25,60 +25,87 @@ type Result<T> = std::result::Result<T, Error>;
 
 fn main() {
     match run() {
-    	Ok(_) => println!("Program completed without errors"),
-    	Err(e) => eprintln!("Program errored: {}", e)
+        Ok(_) => println!("Program completed without errors"),
+        Err(e) => {eprintln!("Program errored: {}", e); error!("Error: {}", e) }
     }
 }
 
 fn run() -> Result<()> {
-	let channel = load_toml()?.into_channel();
-	info!("loaded channel: {}", channel);
-	let request = reqwest::Client::new();
-	trace!("created reqwest client");
-	let alltime_url = format!("{}/points/{}/alltime/1000", BASE_URL, channel);
-	let top_url  = format!("{}/points/{}/top/1000", BASE_URL, channel);
-	let response: Alltime = request.get(&top_url)
-								   .send()?
-								   .json()?;
-	info!("received response from streamelements api");
-	let today = Utc::today()
-					.format("%d-%m-%Y");
+    let config = load_toml()?;
+    let channel = config.channel();
+    info!("loaded channel: {}", channel);
+    let request = reqwest::Client::new();
+    trace!("created reqwest client");
 
-	info!("date for filename: {}", today);
-	let mut csv = WriterBuilder::new()
-								.has_headers(false)
-								.from_path(format!("{}-top-points.csv", today))?;
-	info!("successfully created csv writer");
-	write_to_csv(&mut csv, &response.users())?;
-	if response._total < 1000 {
-		return Ok(())
-	}
-	// Rounded up integer division, rust rounds towards
-	// zero as that is what llvm does.
-	// We are requesting the max number of records, 1000
-	let offset_count = ((response._total - 1) / 1000) + 1;
-	for offset in 2..offset_count+1 {
-		let resp: Alltime = request.get(&format!("{}?offset={}", top_url, offset))
-			   			  		   .send()?
-			   			  		   .json()?;
-		info!("received response from streamelements api");
-		write_to_csv(&mut csv, &resp.users())?;
-		info!("successfully wrote to csv");
-	}
-	Ok(())
+    let alltime_url = format!("{}/points/{}/alltime/1000", BASE_URL, channel);
+    let top_url = format!("{}/points/{}/top/1000", BASE_URL, channel);
+    
+    let response: Alltime = request.get(&top_url).send()?.json()?;
+    info!("received response from streamelements api");
+    
+    let today = Utc::today().format("%d-%m-%Y");
+    info!("date for filename: {}", today);
+    
+    let mut csv = WriterBuilder::new()
+        .has_headers(false)
+        .from_path(format!("{}-top-points.csv", today))?;
+    info!("successfully created csv writer");
+    write_to_csv(&mut csv, &response.users())?;
+
+    // We request 1000 initially, if the total is less
+    // then we found them all
+    if response._total < 1000 {
+        return Ok(());
+    }
+    // Rounded up integer division, rust rounds towards
+    // zero as that is what llvm does.
+    // We are requesting the max number of records, 1000
+    let offset_count = ((response._total - 1) / 1000) + 1;
+    for offset in 2..offset_count + 1 {
+        let offset = offset * 1000;
+        let resp: Alltime = request
+            .get(&format!("{}?offset={}", top_url, offset))
+            .send()?
+            .json()?;
+        info!("received response from streamelements api");
+
+        // Not sure how to build around this without duplicating code
+        // looks very messy
+        match config.cutoff() {
+            Some(cutoff) => {
+                let last_point = resp.users().last().unwrap().points;
+                if last_point < cutoff {
+                    let filtered: Vec<User> = resp.into_users()
+                                       .into_iter()
+                                       .filter(|user| user.points > cutoff)
+                                       .collect();
+                    write_to_csv(&mut csv, filtered.as_slice())?;
+                    break
+                }
+            }
+            None => {}
+        }
+        write_to_csv(&mut csv, &resp.users())?;
+        info!("successfully wrote to csv");
+    }
+    Ok(())
 }
 
+/// Convenience function because Toml doesn't support reading from a file
+/// serde_json
 fn load_toml() -> Result<ActualConfig> {
-	use std::io::Read;
-	let mut file = File::open("./config.toml")?;
-	let mut str_bufr = String::new();
-	file.read_to_string(&mut str_bufr)?;
-	Ok(toml::from_str(&str_bufr)?)
+    use std::io::Read;
+    let mut file = File::open("./config.toml")?;
+    let mut str_bufr = String::new();
+    file.read_to_string(&mut str_bufr)?;
+    Ok(toml::from_str(&str_bufr)?)
 }
 
+/// Convenience function to serialize to a CSV via an iterator.
+/// TODO: Generics
 fn write_to_csv(csv: &mut csv::Writer<File>, users: &[User]) -> Result<()> {
-	for user in users {
-		csv.serialize(user)?;
-	}
-	Ok(())
+    for user in users {
+        csv.serialize(user)?;
+    }
+    Ok(())
 }
